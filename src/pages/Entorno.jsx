6eebea2589aa590ec9bc409a036fbf08860ec9e6
kakomoma <alejandromorenomartin1990@gmail.com>
2025-09-29 00:06:09 +0200

    añadir soporte multilingue y actulizacion de metas

---
+++ src/pages/Entorno.jsx
@@ -0,0 +1,22 @@
+import { Helmet } from "react-helmet-async";
+import { useTranslation } from "react-i18next";
+
+export default function Entorno() {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <>
+      <Helmet>
+        <html lang={i18n.language} />
+        <title>{t("entorno.meta.title")}</title>
+        <meta name="description" content={t("entorno.meta.description")} />
+        <meta property="og:title" content={t("entorno.meta.title")} />
+        <meta property="og:description" content={t("entorno.meta.description")} />
+        <meta property="og:image" content="/src/assets/icons/favicon.svg" />
+      </Helmet>
+
+      <h1>{t("entorno.title")}</h1>
+      <p>{t("entorno.description")}</p>
+    </>
+  );
+}
