78f607f1af466c0f675510989b94f2fc05b47e09
kakomoma <alejandromorenomartin1990@gmail.com>
2025-09-29 10:38:01 +0200

    cambios en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Casa from "./pages/Casa";
 import Entorno from "./pages/Entorno";
@@ -8,13 +8,13 @@ import Entorno from "./pages/Entorno";
 function App() {
   return (
     <HelmetProvider>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/casa" element={<Casa />} />
           <Route path="/entorno" element={<Entorno />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </HelmetProvider>
   );
 }
